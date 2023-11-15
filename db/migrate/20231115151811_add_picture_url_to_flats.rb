368c2e017f3c990bd1809cc85a5437b6c04f3417
Digby Hebb <digby.hebb@gmail.com>
2023-11-15 21:51:49 +0000

    simple-airbnb complete

---
+++ db/migrate/20231115151811_add_picture_url_to_flats.rb
@@ -0,0 +1,5 @@
+class AddPictureUrlToFlats < ActiveRecord::Migration[7.1]
+  def change
+    add_column :flats, :image_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts "clearing db"
+Flat.destroy_all
+
+puts 'Generating 5 flats'
+
+building_type = ['Flat', 'Condo', 'House', 'Villa']
+images = [
+  'https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1625602812206-5ec545ca1231?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1503174971373-b1f69850bded?q=80&w=3313&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1430285561322-7808604715df?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1584738766473-61c083514bf4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+]
+
+5.times do
+  Flat.create!(
+    name: "#{Faker::Address.city} #{building_type.sample}",
+    address: Faker::Address.full_address ,
+    description: Faker::Quote.yoda,
+    price_per_night: (75..200).to_a.sample,
+    number_of_guests: (1..10).to_a.sample,
+    image_url: images.sample
+  )
+end
+
+puts 'done'
